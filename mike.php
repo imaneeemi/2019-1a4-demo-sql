59a363dae700867c00fabd03081b1d6fb301642f
bunkermaster <yann.le-scouarnec@hetic.net>
2017-05-22 15:28:57 +0200

    Rajout de commentaires

--- mike.php
+++ mike.php
@@ -1,5 +1,7 @@
 <?php
+// inclusion du fichier de connexion mysql PDO
 require_once "connect.php";
+// requete SQL simple, un SELECT
 $sql = "SELECT
   `id`,
   `prenom`,
@@ -11,15 +13,21 @@ FROM
 WHERE
   1
 ";
+// premiere etape, le prepare
 $stmt = $pdo->prepare($sql);
-// pas de bind
+// deuxieme etape, le bind
+// ici, pas de bind 😖
+// troisieme etape, execution de la requete
 $stmt->execute();
+// quatrieme etape verification des erreurs
 if($stmt->errorCode() !== '00000'){
     die($stmt->errorInfo()[2]);
 }
 //$row = $stmt->fetch(PDO::FETCH_OBJ);
 //var_dump($row);
 //echo $row->nom;
+// etape final, le fetch, dans une boucle while
 while(false !== $row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    // affichage du contenu de la table
     echo $row['prenom']." ".$row['nom']." ".$row['adresse_email'].PHP_EOL;
 }
